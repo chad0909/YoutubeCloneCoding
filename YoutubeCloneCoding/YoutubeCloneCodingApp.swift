c83c17f29ebe69768482d3a5aeadddba9e229c24
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2023-03-23 19:49:27 +0900

    Initial Commit

---
+++ YoutubeCloneCoding/YoutubeCloneCodingApp.swift
@@ -0,0 +1,20 @@
+//
+//  YoutubeCloneCodingApp.swift
+//  YoutubeCloneCoding
+//
+//  Created by 077tech on 2023/03/23.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeCloneCodingApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
